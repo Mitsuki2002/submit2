c260103efc602b5f43969c479b088517eca49cc8
EC2 Default User <137010302+Mitsuki2002@users.noreply.github.com>
2023-09-14 12:55:02 +0000

    本の感想を登校するためのサイト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,19 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
-    @books = Book.all 
+    @books = Book.all
     # book = Book.find(params[:id])  # データ（レコード）を1件取得
     # book.destroy  # データ（レコード）を削除
-    # redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+    # redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
   def show
-    @book = Book.find(params[:id])  
+    @book = Book.find(params[:id])
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -25,30 +25,30 @@ class BooksController < ApplicationController
       render :edit
     end
     # book.update(book_params)
-    # redirect_to book_path(book.id)  
+    # redirect_to book_path(book.id)
   end
-  
+
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
     flash[:notice] = "Book was successfully created."
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
-  
-  
+
+
 
   # def new
   #   @Book = Book.new
   # end
-  
+
   # def index
-  #   @books = Book.all 
+  #   @books = Book.all
   # end
 
   # def edit
   # end
-  
-  
+
+
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     @book = Book.new(book_params)
@@ -62,8 +62,8 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      
-      @books = Book.all 
+
+      @books = Book.all
       render :index
     end
   end
